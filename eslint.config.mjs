90f9dcf1f724d518b80af3a3fafbac3b9c4c57f2
hashemalo <hhaasshhemm@gmail.com>
2026-01-17 19:55:43 -0500

    f

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,32 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable strict TypeScript rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-interface": "off",
+      "@typescript-eslint/ban-types": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+      
+      // Disable React rules
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "react/display-name": "off",
+      "react/prop-types": "off",
+      
+      // Disable Next.js rules
+      "@next/next/no-img-element": "off",
+      "@next/next/no-html-link-for-pages": "off",
+      
+      // Disable general rules
+      "no-console": "off",
+      "no-debugger": "off",
+      "prefer-const": "off",
+      "no-unused-vars": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
